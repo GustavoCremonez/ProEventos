d8747997b10f0ca26b03682ddb2205024d80fc48
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-12-08 22:16:56 -0300

    Trabalhando com requisição get para pegar os eventos no front

---
+++ Back/ProEventos/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento Get(int id)
+        {
+            return _context.Eventos.Where(x => x.EventoId == id).FirstOrDefault();
+        }
+    }
+}
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,10 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
+  public eventos: any;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:44391/api/Eventos').subscribe({
+      next: this.handleUpdateEventos.bind(this),
+      error: this.handleError.bind(this),
+    });
+  }
+
+  public handleUpdateEventos(response: any): void {
+    this.eventos = response;
+  }
+
+  public handleError(error: Error): void {
+    console.error(error);
+  }
 }
