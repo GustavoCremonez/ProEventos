331313bf09f9bbd5a05fe695d517eb5c937de66b
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-12-08 21:42:03 -0300

    Iniciando projeto

---
+++ Back/ProEventos/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento Get(int id)
+        {
+            return _context.Eventos.Where(x => x.EventoId == id).FirstOrDefault();
+        }
+    }
+}
