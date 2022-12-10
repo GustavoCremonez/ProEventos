cbbc0d0e63188073af5a7bc42fb2cb13abeb6f93
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-12-09 23:17:39 -0300

    Alterando tela de eventos adicionando-a campo de filtro e melhorando o layout

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
-  public eventos: any;
+  public eventos: any = [];
+  public filteredEvents: any = [];
+
+  private _filterList: string = '';
+  hideImage: boolean = false;
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(filter: string) {
+    this._filterList = filter;
+
+    this.filteredEvents = this.filterList
+      ? this.handleFilterEvents(this.filterList)
+      : this.eventos;
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -24,9 +40,25 @@ export class EventosComponent {
 
   public handleUpdateEventos(response: any): void {
     this.eventos = response;
+    this.filteredEvents = response;
   }
 
   public handleError(error: Error): void {
     console.error(error);
+  }
+
+  public handleHideImage() {
+    this.hideImage = !this.hideImage;
+  }
+
+  public handleFilterEvents(filter: string): any {
+    filter = filter.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filter) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filter) !== -1 ||
+        evento.dataEvento.indexOf(filter) !== -1
+    );
   }
 }
