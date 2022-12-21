22d7701187aa9269d70145b04c3b3526a59ad778
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-12-20 21:30:15 -0300

    Adicionando tema ao bootstrap e melhorando o component de título,

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,10 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css'],
+  styleUrls: ['./titulo.component.scss'],
 })
 export class TituloComponent implements OnInit {
   @Input() titulo: string = '';
+  @Input() subtitulo: string = '';
+  @Input() iconClass: string = '';
+  @Input() botaoListar: boolean = false;
 
   constructor() {}
 
