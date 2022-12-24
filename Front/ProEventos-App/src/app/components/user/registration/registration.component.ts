33f5dc01355aef8c44e8b30ca3d332fe9faf92b0
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-12-23 21:39:01 -0300

    Adicionando o reactive form para o formulario de registro e criando class para validação de campos

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorField } from 'src/app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent {
+  form: FormGroup;
 
+  get formControls(): any {
+    return this.form.controls;
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha'),
+    };
+
+    this.form = this.formBuilder.group(
+      {
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmarSenha: ['', Validators.required],
+      },
+      formOptions
+    );
+  }
 }
